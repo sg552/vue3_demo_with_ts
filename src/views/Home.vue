df6202409d2d47cefeb69969c48769a80b243cfa
申思维 <shensiwei@sina.com>
2021-10-06 16:16:29 +0800

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <p> hi {{name}} </p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+ name: 'App',
+ setup () {
+  const name = "Jim"
+
+  return {
+    name
+  }
+ }
 });
 </script>
